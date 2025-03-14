a21e3a666a83cd1f1a7e180c48f849b924a6baa9
Mike Cecconello <mikececco2000@gmail.com>
2025-03-14 15:16:15 +0100

    Add 'use client' directive to Order Page Component
    - Introduced 'use client' directive in the Order Page component to enable client-side rendering.
    - This change is aimed at improving the component's interactivity and performance in a React environment.

--- app/orders/[id]/page.jsx
+++ app/orders/[id]/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from "next/link";
 import { supabase } from "../../utils/supabase";
 import { StatusBadge, PaymentBadge, ShippingToggle, StatusSelector } from "../../components/OrderActions";
