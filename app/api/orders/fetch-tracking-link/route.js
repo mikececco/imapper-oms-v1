3c76cfeb4d1219457aa5d6404dbaa54c18b6eb42
Mike Cecconello <mikececco2000@gmail.com>
2025-03-19 16:24:14 +0100

    Update Supabase Environment Variable References in Fetch Tracking Link API
    - Changed the Supabase environment variable references in the fetch-tracking-link route to align with the updated naming convention, enhancing consistency across the application.
    - Ensured that the necessary environment variables are still validated before client initialization for improved error handling.

--- app/api/orders/fetch-tracking-link/route.js
+++ app/api/orders/fetch-tracking-link/route.js
@@ -4,8 +4,8 @@ import { fetchShippingDetails } from '../../../utils/sendcloud';
 
 export async function GET(request) {
   try {
-    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    const supabaseUrl = process.env.NEXT_SUPABASE_URL;
+    const supabaseKey = process.env.NEXT_SUPABASE_ANON_KEY;
 
     if (!supabaseUrl || !supabaseKey) {
       console.error('Missing Supabase environment variables');
